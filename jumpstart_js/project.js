7b8a425008ac519d48f640e7f3b8fc5a94374d4d
sgpl <sharad45@gmail.com>
2014-03-15 18:58:15 +0300

    Update tasks from standalone variables to tasks in an array

--- jumpstart_js/project.js
+++ jumpstart_js/project.js
@@ -1,5 +1,18 @@
+var tasks; 
+
+tasks = [
+	"Pay boll", 
+	"Write novel", 
+	"Walk dog"
+];
+
+
+
+/* //version 1
 var task1, task2, task3;
 
 task1 = "Pay phone bill"; 
 task2 = "Complete this book"; 
 task3 = "Do something interesting with my life"; 
+*/
+

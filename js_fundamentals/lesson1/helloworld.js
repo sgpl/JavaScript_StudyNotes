8acc3c1ef092214fc16b500b77c34685ed405298
sgpl <sharad45@gmail.com>
2014-10-30 17:57:35 -0400

    Three data types used. Numbers, strings, and boolean

--- js_fundamentals/lesson1/helloworld.js
+++ js_fundamentals/lesson1/helloworld.js
@@ -1,2 +1,17 @@
-alert("hello world"); 
+var myvariable; // declaring variable // currently value is undefined
+myvariable = "Wow this is so fancy"; // assignment statement or variable initialization
+var hello = "Hello World";
+// alert("hello world");
+alert(hello); 
 alert("Hello Javascript Fundamentals");
+alert(myvariable);
+
+// camelcase - first word is lower case, next word and following are upper cased
+// can use under score too, but this isn't javascript convention
+// this would work too: 
+// myVariable = "Hello world"; 
+// myvaRiable = "Hello world";
+
+var ten = 10; 
+var pi = 3.14; 
+var isBoolean = false; 

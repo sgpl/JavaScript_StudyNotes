5d3ea796c7071d3408b5ca8dce2fd8335546aa71
sgpl <sharad45@gmail.com>
2014-10-31 18:18:17 -0400

    Add new parameter to function

--- js_fundamentals/lesson1/script_functions.js
+++ js_fundamentals/lesson1/script_functions.js
@@ -12,16 +12,36 @@ bar = bar * 8; // 56
 // function 
 // can enter parameters within the paranthesis if we want. 
 // will help us pass data to the function 
-function doSomething(paramOne) {
+
+// function doSomething(paramOne) {
+// 	paramOne = paramOne + 3; 
+// 	paramOne = paramOne + 1;
+// 	paramOne = paramOne * 8;
+// 	// paramOne = (paramOne + 3 + 1) * 8; 
+// 	// returns undefined as doSomething currently doesn't return stuff
+// 	return paramOne;
+// }
+
+// var foo1 = doSomething(2);
+// var bar1 = doSomething(3);
+// // alert("foo is" + foo1 " and bar is " + bar1);
+// alert("foo1 is " + foo1);
+
+// ----------------
+
+function doSomething(paramOne, paramTwo) {
 	paramOne = paramOne + 3; 
 	paramOne = paramOne + 1;
 	paramOne = paramOne * 8;
 	// paramOne = (paramOne + 3 + 1) * 8; 
 	// returns undefined as doSomething currently doesn't return stuff
-	return paramOne;
+	return paramOne * paramTwo;
 }
 
-var foo1 = doSomething(2);
-var bar1 = doSomething(3);
+var foo1 = doSomething(2, 2);
+var bar1 = doSomething(3, 2);
 // alert("foo is" + foo1 " and bar is " + bar1);
-alert("foo1 is " + foo1);
+alert("foo1 is " + foo1 + " and bar1 is " + bar1);
+
+
+// can create functions and assign them to variables in javascript: 

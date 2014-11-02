d25104dcabd0c773336eb0f018837732f32cd10f
sgpl <sharad45@gmail.com>
2014-11-02 17:31:32 -0500

    Using a funciton inside of a function

--- js_fundamentals/lesson1/script_functions.js
+++ js_fundamentals/lesson1/script_functions.js
@@ -49,16 +49,26 @@ bar = bar * 8; // 56
 
 // anonymous function. doesn't have a name 
 // can assign it to a variable 
-var doSomething = function(paramOne, paramTwo) {
+var doSomething = function(paramOne, paramTwo, fn) {
 	paramOne = paramOne + 3; 
 	paramOne = paramOne + 1;
 	paramOne = paramOne * 8;
 	// paramOne = (paramOne + 3 + 1) * 8; 
 	// returns undefined as doSomething currently doesn't return stuff
-	return paramOne * paramTwo;
+	return fn(paramOne, paramTwo);
 };
 
-var foo1 = doSomething(2, 2);
-var bar1 = doSomething(3, 2);
-// alert("foo is" + foo1 " and bar is " + bar1);
-alert("foo1 is " + foo1 + " and bar1 is " + bar1);
+function sum(paramOne, paramTwo) {
+	return paramOne + paramTwo;
+}
+
+function product(paramOne, paramTwo) {
+	return paramOne * paramTwo;
+}
+
+// calling sum funciton inside of doSomething 
+var foo1 = doSomething(2, 2, sum);
+var bar1 = doSomething(3, 2, product);
+
+alert(foo1);
+alert(bar1);

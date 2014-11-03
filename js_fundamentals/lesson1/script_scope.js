306575311837d4adaf3583f1e7f730b4834be51d
sgpl <sharad45@gmail.com>
2014-11-03 12:16:22 -0500

    local scope

--- js_fundamentals/lesson1/script_scope.js
+++ js_fundamentals/lesson1/script_scope.js
@@ -8,12 +8,34 @@
 // 	- Global scope is default 
 // 		- every variable and function so far is global 
 
+
+// var globalVar = "This is a global variable";
+
+// var globalFunction = function(){
+// 	alert(globalVar);
+// 	globalVar = "Value of globalVar has been modified";
+// };
+
+// globalFunction();
+// alert(globalVar);
+
+
+//----------------------
+// scope
+// 	- Local (of functional scope)
+// 		- variables and functions are limited to the function they are defined in. 
+// 		- variables and functions cannnot be accesses from outside the function. 
+
+
 var globalVar = "This is a global variable";
 
 var globalFunction = function(){
-	alert(globalVar);
+	var localVar = "This is a local variable"
+	alert(localVar);
 	globalVar = "Value of globalVar has been modified";
 };
 
 globalFunction();
+
+alert(localVar); // this results in an error as this is a local variable to the global function
 alert(globalVar);
